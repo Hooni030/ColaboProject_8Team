c4e4214035baff354c8663daba82ee38ab9ebad2
제갈도원 <insopa01@naver.com>
2026-03-19 11:58:32 +0900

    [Test] 한글 테스트 스크립트

---
+++ TestProject/Assets/Scripts/Test_dw.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Test_dw : MonoBehaviour
+{
+    // 한글 테스트
+    // 크아아ㅏ아악
+    // 으아아가
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
