9a055c4b443fc04a708855155b5e5c078596ee26
Hooni0_0 <startrack0515@gmail.com>
2026-03-19 12:53:33 +0900

    [fix] 인코딩 에디터 설정 파일 추가
    - .editorconfig

---
+++ TestProject/Assets/Scripts/Script_Rider.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Script_Rider : MonoBehaviour
+{
+    // 한글 테스트
+    // 한글 테스트 2
+    private string test = "한글 테스트";
+}
---
+++ TestProject/Assets/Scripts/Script_VS.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Script_VS : MonoBehaviour
+{
+    // 한글 테스트
+    // 한글 테스트 2
+    private string test = "한글 테스트";
+}
