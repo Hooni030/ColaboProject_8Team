28d49628d4e6e9d1db2be2c4b391f581929167bc
Hooni0_0 <startrack0515@gmail.com>
2026-03-19 11:54:05 +0900

    [Test] 테스트용 프로젝트 생성 260319
    - 테스트 프로젝트
    - 테스트 스크립트 추가

---
+++ TestProject/Assets/Scripts/TestScripts.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TestScripts : MonoBehaviour
+{
+    // 주석 테스트 한글 테스트
+    // 가나다라마바사
+    
+    private string test = "Test, 테스트";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
